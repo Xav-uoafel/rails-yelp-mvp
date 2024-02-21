846f2d4dc052ad3a144d94ce66ef0d0137370bfd
Xavier Le Faou <lefaoux@hotmail.fr>
2024-02-21 16:51:11 +0100

    Yelp
    Signed-off-by: Xavier Le Faou <lefaoux@hotmail.fr>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'Droping database...'
+Restaurant.destroy_all
+puts 'restaurant table is empty'
+puts 'Creating restaurants...'
+Restaurant.create!(:name => "Dishoom", :address => "7 Boundary St, London E2 7JE", :category => "chinese")
+Restaurant.create!(:name => "Pizza East", :address => "56A Shoreditch High St, London E1 6PQ", :category => "italian")
+Restaurant.create!(:name => "Sushi Samba", :address => "110 Bishopsgate, London EC2N 4AY", :category => "japanese")
+Restaurant.create!(:name => "Le Gavroche", :address => "43 Upper Brook St, London W1K 7QR", :category => "french")
+Restaurant.create!(:name => "La Cuisine d'Em'", :address => "1 Rue des Pokeballs, 31000 Toulouse", :category => "french")
+puts '5 restaurants created'
